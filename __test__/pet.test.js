8b9e461c71d0f330559ce86d5af1c164537e463f
Benj <ben.speight@outlook.com>
2023-03-12 19:36:43 +0000

    hunger & fitness

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -21,9 +21,22 @@ describe('constructor', () => {
 });
 
 describe('growUp', () => {
+    
   it('increments the age by 1', () => {
     const pet = new Pet('Fido');
     pet.growUp();
     expect(pet.age).toEqual(1);
+  });
+
+  it('increments the hunger value by 5', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  });
+
+  it('decrements the fitness value by 3', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
   });
 });
